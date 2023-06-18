0c161e7a00dd20ada784bbaba4d6c316630cef9c
Alaa jawhar <alaajawhar123@gmail.com>
2023-06-18 16:32:39 +0300

    change of heart based on EM

--- auth-server/src/main/java/com/ajp/yummerauthserver/config/DefaultSecurityConfig.java
+++
@@ -1,7 +0,0 @@
-package com.ajp.yummerauthserver.config;
-
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-@EnableWebSecurity
-public class DefaultSecurityConfig {
-}
